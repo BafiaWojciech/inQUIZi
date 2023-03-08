a77874b3a7d063f1c43d0717c3c55ec00eac60ca
BafiaWojciech <bafia.wojciech.01@gmail.com>
2023-03-08 11:45:42 +0100

    init commit

---
+++ src/main/java/com/bafia/inquizi/InquiziApplication.java
@@ -0,0 +1,13 @@
+package com.bafia.inquizi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bafia/inquizi/InquiziApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bafia.inquizi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquiziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
